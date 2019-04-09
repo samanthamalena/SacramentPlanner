961e841149a7d2ecfd7c30a304685fc0af27fdfb
samanthahancock <han13018@byui.edu>
2019-04-09 15:46:42 -0600

    fixed some issues with Sacraments Create, Edit, etc.

--- SacramentPlanner/Controllers/SacramentsController.cs
+++ SacramentPlanner/Controllers/SacramentsController.cs
@@ -53,7 +53,7 @@ namespace SacramentPlanner.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Date,Conducting,OpenHymn,OpenPrayer,SacramentHymn,Speaker,Subject,CloseHymn,ClosePrayer")] Sacrament sacrament)
+        public async Task<IActionResult> Create([Bind("Id,Date,Conducting,OpenHymn,OpenPrayer,SacramentHymn,IntermediateHymn,CloseHymn,ClosePrayer")] Sacrament sacrament)
         {
             if (ModelState.IsValid)
             {
@@ -85,7 +85,7 @@ namespace SacramentPlanner.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,Conducting,OpenHymn,OpenPrayer,SacramentHymn,IntermediateHymn,Speaker,Subject,CloseHymn,ClosePrayer")] Sacrament sacrament)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,Conducting,OpenHymn,OpenPrayer,SacramentHymn,IntermediateHymn,CloseHymn,ClosePrayer")] Sacrament sacrament)
         {
             if (id != sacrament.Id)
             {
